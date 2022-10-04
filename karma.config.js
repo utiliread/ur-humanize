3825b66eeb41df42ba1c67e23d6ac06c82b5310e
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-10-04 14:21:27 +0200

    Update dependencies

--- karma.config.js
+++ karma.config.js
@@ -1,34 +1,33 @@
 const tsconfig = require("./tsconfig.json");
 
 module.exports = function (config) {
-    config.set({
-        frameworks: ["mocha", "chai", "karma-typescript"],
-        files: [
-            "src/**/*.ts"
-        ],
-        preprocessors: {
-            "src/**/*.ts": ["karma-typescript"]
+  config.set({
+    frameworks: ["mocha", "chai", "karma-typescript"],
+    files: ["src/**/*.ts"],
+    preprocessors: {
+      "src/**/*.ts": ["karma-typescript"],
+    },
+    reporters: ["progress", "karma-typescript"],
+    browsers: ["ChromeHeadless", "ChromeDebugging", "Chrome"],
+    customLaunchers: {
+      ChromeDebugging: {
+        base: "Chrome",
+        flags: ["--remote-debugging-port=9222"],
+      },
+    },
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        sourceMap: tsconfig.compilerOptions.sourceMap,
+        acornOptions: {
+          ecmaVersion: "latest",
         },
-        reporters: ["progress", "karma-typescript"],
-        browsers: ["ChromeHeadless", "ChromeDebugging"],
-        customLaunchers: {
-            ChromeDebugging: {
-                base: "Chrome",
-                flags: ["--remote-debugging-port=9222"]
-            }
-        },
-        karmaTypescriptConfig: {
-            bundlerOptions: {
-                transforms: [
-                    require("karma-typescript-es6-transform")()
-                ],
-                entrypoints: /\.spec\.ts$/,
-                sourceMap: tsconfig.compilerOptions.sourceMap
-            },
-            coverageOptions: {
-                instrumentation: false
-            },
-            tsconfig: "./tsconfig.cjs.json"
-        }
-    })
-}
+        transforms: [require("karma-typescript-es6-transform")()],
+      },
+      coverageOptions: {
+        instrumentation: false,
+      },
+      tsconfig: "./tsconfig.cjs.json",
+    },
+  });
+};
